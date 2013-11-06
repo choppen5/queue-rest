03457b45e00513d65a21c141a01d6c3842debf64
choppen5 <choppen5@yahoo.com>
2013-11-05 16:10:26 -0800

    added dequeuing

--- queue-rest.rb
+++ queue-rest.rb
@@ -9,6 +9,7 @@ account_sid = ENV['twilio_account_sid']
 auth_token =  ENV['twilio_account_token']
 app_id =  ENV['twilio_app_id']
 
+caller_id = '+14423336837'
 
 @client = Twilio::REST::Client.new(account_sid, auth_token)
 
@@ -26,6 +27,52 @@ post '/voice' do
     puts "response text = #{response.text}"
     response.text
 end
+
+#http
+get '/connect-to-agent' do
+
+  #pass in agent targets number
+  agent_num = params['agent_num']
+
+  #rest call to start a call, gahter response URL = 'get-response'
+  @client = Twilio::REST::Client.new(account_sid, auth_token)
+
+  @client.account.calls.create(:from => caller_id, :to => agent_num, :url => URI.escape("https://macbook.ngrok.com/confirm-agent-reservation"))
+   
+end
+
+post '/confirm-agent-reservation' do
+
+  #1. Do a gather  - Action will be = dequeue-connection
+    response = Twilio::TwiML::Response.new do |r|
+
+    # call screen
+    r.Pause "1"
+    r.Gather(:action => URI.escape("/dequeue-connection"), :timeout => "10", :numDigits => "1") do |g|
+      g.Say("You have a customer ready, press 1 to talk to them")
+    end
+
+    # no key was pressed
+    r.hangup
+
+    return r.text
+
+  end
+
+
+end
+
+post '/dequeue-connection' do
+  puts params
+
+  #rest dequeue front of queue.. 
+  return "<Response><Dial><Queue>WelcomeQueue</Queue></Dial></Response>"
+
+ 
+
+end 
+
+
 
 
 
